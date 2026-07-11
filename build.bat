9e69d1658d045a19774b26f0ec48f29977320215
Flynn Crochon <flynncrochon@gmail.com>
2026-07-11 13:01:54 +1000

    Build info

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ set "ROOT=%~dp0"
 if exist "%ROOT%dist" rmdir /s /q "%ROOT%dist"
 cmake -S "%ROOT%." -B "%ROOT%build" -DCMAKE_BUILD_TYPE=Release || exit /b 1
 cmake --build "%ROOT%build" --config Release || exit /b 1
-cmake --install "%ROOT%build" --prefix "%ROOT%dist/fastgrid" --config Release || exit /b 1
+cmake --install "%ROOT%build" --prefix "%ROOT%dist" --config Release || exit /b 1
 
 echo [build] dist -^> %ROOT%dist\fastgrid
 exit /b 0
